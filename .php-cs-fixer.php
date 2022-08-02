cd3d41d574ed9117ead5b4b0f8e950be21d83c65
Deeka Wong <huangdijia@gmail.com>
2022-08-02 11:09:11 +0800

    format

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/compoships.
  *
  * @link     https://github.com/friendsofhyperf/compoships
- * @document https://github.com/friendsofhyperf/compoships/blob/3.x/README.md
+ * @document https://github.com/friendsofhyperf/compoships/blob/master/README.md
  * @contact  huangdijia@gmail.com
  */
 $header = <<<'EOF'
 This file is part of friendsofhyperf/compoships.
 
 @link     https://github.com/friendsofhyperf/compoships
-@document https://github.com/friendsofhyperf/compoships/blob/master/README.md
+@document https://github.com/friendsofhyperf/compoships/blob/main/README.md
 @contact  huangdijia@gmail.com
 EOF;
 
--- src/Database/Eloquent/Model.php
+++ src/Database/Eloquent/Model.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/compoships.
  *
  * @link     https://github.com/friendsofhyperf/compoships
- * @document https://github.com/friendsofhyperf/compoships/blob/master/README.md
+ * @document https://github.com/friendsofhyperf/compoships/blob/main/README.md
  * @contact  huangdijia@gmail.com
  */
 namespace FriendsOfHyperf\Compoships\Database\Eloquent;
